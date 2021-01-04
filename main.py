e103e4f59881184c3e98b3c5ba11709838b650cd
Denis <denybf.wrk@gmail.com>
2021-01-04 17:07:45 +0300

    added main functions for list of commands

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-import datetime
+from datetime import datetime
 import requests
 import config
 
@@ -12,10 +12,43 @@ print(url)
 bot = telebot.TeleBot(config.bot_token)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def welcome(message):
-    answer = """Привет!\nЯ буду присылать тебе погоду на день каждое утро. \nВыбери город и каждый день в 9 утра будешь получать свежий прогноз :) """
-    bot.send_message(message.chat.id, answer)
+def log(message, answer):
+    print("\n -------- ")
+    print(datetime.now())
+    print("Сообщение от {0} {1}. (id = {2}) \nТекст - {3}".format(message.from_user.first_name, message.from_user.last_name, str(message.from_user.id), message.text))
+    print("Ответ - ", answer)
+    print("\n -------- ")
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    name = message.from_user.first_name
+    answer = """Привет, {}!\nЯ буду присылать тебе погоду на день каждое утро. 
+Выбери город и каждый день в 9 утра будешь получать свежий прогноз :) """.format(name)
+    log(message, answer)
+    bot.send_message(message.from_user.id, answer)
+    get_help(message)
+
+
+@bot.message_handler(commands=['help'])
+def get_help(message):
+    help_message = """Прежде всего нужно выбрать город, для которого будет находиться прогноз.
+Для этого отправь мне название своего города.
+    """
+    log(message, help_message)
+    bot.send_message(message.from_user.id, help_message)
+
+
+@bot.message_handler(commands=['city'])
+def set_city(message):
+    bot.send_message(message.from_user.id, 'Отправь мне название города.')
+
+
+@bot.message_handler(content_types=['text'])
+def get_city(message):
+    city = message.text
+    bot.send_message(message.from_user.id, 'Ты отправил {}. Теперь я буду искать прогноз погоды для этого города.'.format(city))
+    # TODO: парсинг города в openweather, разбор json на нужные данные, формирование ответа.
 
 
 if __name__ == '__main__':
